46091178edbd5c973a5b561ff958519e512b2381
tvaisanen <vaisanen.toni@gmail.com>
2022-07-15 22:38:49 +0300

    Add structure

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,22 @@
 
 use reqwest;
 use scraper::{Html, Selector};
+use std::io::Write;
 use std::{env, error, fs, io};
 
-use tui::backend::CrosstermBackend;
+use tui::backend::{Backend, CrosstermBackend};
 use tui::layout::{Constraint, Direction, Layout};
 use tui::style::{Color, Modifier, Style};
 use tui::widgets::{Block, Borders, List, ListItem, Widget};
-use tui::Terminal;
+use tui::{Frame, Terminal};
 
-#[derive(Debug)]
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+#[derive(Clone, Debug)]
 struct SearchResult {
     href: String,
     summary: String,
@@ -18,24 +25,23 @@ struct SearchResult {
 
 const DUCKDUCKGO: &str = "https://html.duckduckgo.com/html?q=";
 
+#[derive(Clone, Debug)]
 struct App {
-    search_results: Vec<SearchResult>
+    query_string: String,
+    search_results: Vec<SearchResult>,
 }
 
 impl Default for App {
     fn default() -> App {
         App {
-            search_results: Vec::new()
+            query_string: "".to_string(),
+            search_results: Vec::new(),
         }
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn error::Error>> {
-    let stdout = io::stdout();
-
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
 
     let args: Vec<String> = env::args().collect();
 
@@ -43,25 +49,61 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
         panic!("Invalid arguments!");
     }
 
+    enable_raw_mode()?;
+
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let mut app = App::default();
+
+    app.query_string = args[1].clone();
+
+
+    let res = run_app(&mut terminal, app);
+
+    disable_raw_mode()?;
+
+    // execute!(
+    //     terminal.backend_mut(),
+    //     LeaveAlternateScreen,
+    //     DisableMouseCapture
+    // )?;
+
+    // terminal.show_cursor()?;
+
+    if let Err(err) = res.await {
+        println!("{:?}", err)
+    }
+
+    Ok(())
+}
+
+async fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+
     let client = reqwest::Client::new();
 
-    let query = [DUCKDUCKGO.to_string(), args[1].to_string()].concat();
+    let query = [DUCKDUCKGO.to_string(), app.query_string.clone()].concat();
 
-    // println!("{:?}", query);
+    println!("{:?}", query);
 
-    // let response = client.post(query).send().await.unwrap();
+    let response = client.post(query).send().await.unwrap();
 
-    // println!("{:?}", response);
+    println!("{:?}", response);
 
-    // let response_body = match response.status() {
-    //     reqwest::StatusCode::OK => response.text_with_charset("utf-8").await.unwrap(),
-    //     _ => {
-    //         println!("{:?}", response.text_with_charset("utf-8").await.unwrap());
-    //         panic!("Error: !");
-    //     }
-    // };
+    let response_body = match response.status() {
+        reqwest::StatusCode::OK => response.text_with_charset("utf-8").await.unwrap(),
+        _ => {
+            println!("{:?}", response.text_with_charset("utf-8").await.unwrap());
+            panic!("Error: !");
+        }
+    };
 
-    let response_body = fs::read_to_string("sample.html").unwrap();
+    // TODO: refactor to query/search
+
+    // let response_body = fs::read_to_string("sample.html").unwrap();
     let document = Html::parse_document(&response_body);
 
     let result_selector = Selector::parse(".result").unwrap();
@@ -69,7 +111,7 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     let result_summary_selector = Selector::parse(".result__snippet").unwrap();
 
     // for each result get
-    let results = document
+    app.search_results = document
         .select(&result_selector)
         .map(|node| {
             let title_node = node.select(&result_title_selector).next().unwrap();
@@ -88,12 +130,30 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
         })
         .collect::<Vec<SearchResult>>();
 
-    let items = results
+    loop {
+        terminal.draw(|f| ui(f, &app))?;
+
+        if let Event::Key(key) = event::read().expect("Key to be readable.") {
+            match key.code {
+                KeyCode::Char(c) => {
+                    // TODO: navigation here
+                    println!("{}", c);
+                }
+                _ => {}
+            };
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    let list_items = app
+        .search_results
+        .clone()
         .into_iter()
         .map(|r| ListItem::new(r.href))
         .collect::<Vec<ListItem>>();
 
-    let list = List::new(items)
+    let list = List::new(list_items)
         .block(
             Block::default()
                 .title("Search Results: page 1")
@@ -103,16 +163,11 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
         .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
         .highlight_symbol(">>");
 
-    terminal.clear()?;
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([Constraint::Percentage(100)].as_ref())
+        .split(f.size());
 
-    terminal.draw(|f| {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints([Constraint::Percentage(100)].as_ref())
-            .split(f.size());
-        f.render_widget(list, chunks[0]);
-    })?;
-
-    Ok(())
+    f.render_widget(list, chunks[0]);
 }
